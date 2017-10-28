65029f48e5c9120a871db87279503b179f68d8ab
Omar M <omar84@gmail.com>
2017-10-28 14:54:36 -0700

    added routes for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  match 'hits_login' => 'hits_counter/hc_sessions#login', :via => [:post]
+  match 'hits_logout' => 'hits_counter/hc_sessions#logout', :via => [:get]
   match 'hits_report' => 'hits_counter/hc_matching_hits#index', :via => [:get]
 end
